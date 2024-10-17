8658adc82e273f5cb987f9bbae539769806eedfc
Aaryan Singh <aaryansingh@Aaryans-Mini.lan>
2024-10-17 17:09:37 +0530

    adding searchBar and icons to Drawer

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,88 +1,105 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
-import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer>
         <Drawer.Screen
-          name="index" 
+          name="index"
           options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
             drawerLabel: "Home",
-            headerTitle: "",
+            headerTitle: "All events",
             headerRight: () => (
-              <Ionicons
-                onPress={() => {
-                  console.log("Search bar");
-                }}
-                name={"search"}
-                size={25}
-                style={{
-                  color: "#007aff",
-                  paddingRight: 15,
-                }}
-              />
-            ),
-          }}
-        />
-         <Drawer.Screen
-          name="day1Events" 
-          options={{
-            drawerLabel: "Day 1",
-            headerTitle: "Day 1",
-            headerRight: () => (
-              <Ionicons
-                onPress={() => {
-                  console.log("Search bar");
-                }}
-                name={"search"}
-                size={25}
-                style={{
-                  color: "#007aff",
-                  paddingRight: 15,
-                }}
-              />
-            ),
-          }}
-        />
-         <Drawer.Screen
-          name="day2Events" 
-          options={{
-            drawerLabel: "Day 2",
-            headerTitle: "Day 2",
-            headerRight: () => (
-              <Ionicons
-                onPress={() => {
-                  console.log("Search bar");
-                }}
-                name={"search"}
-                size={25}
-                style={{
-                  color: "#007aff",
-                  paddingRight: 15,
-                }}
-              />
+              <View style={styles.inputContainer}>
+                <MaterialIcons
+                  name="search"
+                  size={24}
+                  color="gray"
+                  style={styles.icon}
+                  onPress={() => {
+                    console.log("hlo");
+                  }}
+                />
+                <TextInput style={styles.input} placeholder="Search" />
+              </View>
             ),
           }}
         />
         <Drawer.Screen
-          name="day3Events" 
+          name="day1Events"
           options={{
+          drawerIcon:({ color, size })=>(
+            <Ionicons name="bookmark" size={size} color={color}  />
+           ) ,
+            drawerLabel: "Day 1",
+            headerTitle: "Day 1",
+            headerRight: () => (
+              <View style={styles.inputContainer}>
+                <MaterialIcons
+                  name="search"
+                  size={24}
+                  color="gray"
+                  style={styles.icon}
+                  onPress={() => {
+                    console.log("hlo");
+                  }}
+                />
+                <TextInput style={styles.input} placeholder="Search" />
+              </View>
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="day2Events"
+          options={{
+            drawerIcon:({ color, size })=>(
+              <Ionicons name="bookmark" size={size} color={color}  />
+             ) ,
+            drawerLabel: "Day 2",
+            headerTitle: "Day 2",
+            headerRight: () => (
+              <View style={styles.inputContainer}>
+                <MaterialIcons
+                  name="search"
+                  size={24}
+                  color="gray"
+                  style={styles.icon}
+                  onPress={() => {
+                    console.log("hlo");
+                  }}
+                />
+                <TextInput style={styles.input} placeholder="Search" />
+              </View>
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="day3Events"
+          options={{
+            drawerIcon:({ color, size })=>(
+              <Ionicons name="bookmark" size={size} color={color}  />
+             ) ,
             drawerLabel: "Day 3",
             headerTitle: "Day 3",
             headerRight: () => (
-              <Ionicons
-                onPress={() => {
-                  console.log("Search bar");
-                }}
-                name={"search"}
-                size={25}
-                style={{
-                  color: "#007aff",
-                  paddingRight: 15,
-                }}
-              />
+              <View style={styles.inputContainer}>
+                <MaterialIcons
+                  name="search"
+                  size={24}
+                  color="gray"
+                  style={styles.icon}
+                  onPress={() => {
+                    console.log("hlo");
+                  }}
+                />
+                <TextInput style={styles.input} placeholder="Search" />
+              </View>
             ),
           }}
         />
@@ -90,3 +107,28 @@ export default function Layout() {
     </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    paddingHorizontal: 5,
+    paddingVertical: 2,
+    marginRight: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+  },
+});
